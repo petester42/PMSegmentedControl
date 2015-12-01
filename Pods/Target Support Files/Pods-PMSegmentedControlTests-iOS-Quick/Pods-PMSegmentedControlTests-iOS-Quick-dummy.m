09d72e0aac81d8a5cdd49e18ac2c2addda79f270
Pierre-Marc Airoldi <pierremarcairoldi@gmail.com>
2015-11-30 20:15:50 -0500

    remove ignored files

--- Pods/Target Support Files/Pods-PMSegmentedControlTests-iOS-Quick/Pods-PMSegmentedControlTests-iOS-Quick-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_PMSegmentedControlTests_iOS_Quick : NSObject
-@end
-@implementation PodsDummy_Pods_PMSegmentedControlTests_iOS_Quick
-@end
